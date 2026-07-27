c90110e02327a221bcb302a919763239b8313083
kidest <kidesttilahun25@gmail.com>
2026-07-27 22:30:04 +0300

    add name

---
+++ first.py
@@ -0,0 +1,3 @@
+name = "Kidist"
+
+print("My name is:", name)
---
+++ info.py
@@ -0,0 +1,12 @@
+import datetime
+import sys
+
+name = "Kidist"
+
+today = datetime.date.today()
+
+python_version = sys.version
+
+print("Name:", name)
+print("Today's date:", today)
+print("Python version:", python_version)
